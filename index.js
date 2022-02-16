008941b4cb3f189ee6e231298ffb2b7edf9917c1
wwindway <99008488+wwindway@users.noreply.github.com>
2022-02-16 11:15:08 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.wrap').fullpage({
+    navigation:true,
+    anchors:['1page' , '2page' , '3page' , '4page' , '5page']
+});
+
+$('.img1').colorbox({rel:'img1'});
+$('.youtube').colorbox({iframe:true, innerWidth:1280 , innerHeight:720 , rel:'img1'});
